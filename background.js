84594f9ff0901f46ecb0274028ddeb99059117ef
ajayj <ajayjnvbanda@gmail.com>
2021-06-28 12:46:10 +0530

    background script added

---
+++ background.js
@@ -0,0 +1,1 @@
+console.log("background file is running")
---
+++ server.js
@@ -0,0 +1,12 @@
+const express=require('express');
+
+const app=express();
+
+app.get('/',function(req,res){
+    res.send('Hello world!');
+});
+
+app.listen(3000,function(){
+    console.log('server is started in port 3000...');
+});
+
