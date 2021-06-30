84e34d674209e9411b70e29b49fffb09c5563833
ajayj <ajayjnvbanda@gmail.com>
2021-06-30 16:08:22 +0530

    interact local server and background script

--- background.js
+++ background.js
@@ -1,1 +1,21 @@
+
 console.log("background file is running")
+
+var sampleData="hwllo everyone";
+    
+
+
+var jqxhr=$.ajax({
+    type:"POST",
+    url:" http://localhost:3000/products",
+    processData:false,
+    data:sampleData,
+})
+.done(function(data){
+    console.log("data is successfully send from background script to server"+data);
+})
+.fail(function(xhr,statusCode,err){
+    console.log("error in sending data",statusCode);
+    console.log(err);
+});
+
--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 const express=require('express');
-
 const app=express();
+const cors=require('cors');
+
+
+app.use(cors());
+app.use(express.urlencoded());
+app.use(express.json());
+
 
 app.get('/',function(req,res){
     res.send('Hello world!');
+});
+
+app.post('/products',function(req,res){
+    
+   
+    console.log(req.body);
+  
+    res.send(req.body);
+
 });
 
 app.listen(3000,function(){
