a6e596ac399facd173d066d06f220675a9b9750f
piyusha jaiswal <piyushajaiswal14@gmail.com>
2021-07-08 10:24:59 +0530

    conected database in server.js  and set up angular ui router in popup.js

--- content.js
+++ content.js
@@ -1,1 +1,16 @@
-console.log("content file is running")
+//console.log("content file is running")
+let amazonextension = angular.module("amazonextension", ['ui.router']);
+amazonextension.config([
+    '$stateProvider',
+    '$urlRouterProvider',
+    function($stateProvider, $urlRouterProvider) {
+    
+      $stateProvider
+        .state('home', {
+          url: '/home',
+          templateUrl: '/home.html',
+          controller: 'MainCtrl'
+        });
+    
+      $urlRouterProvider.otherwise('home');
+    }]);
--- server.js
+++ server.js
@@ -1,22 +1,21 @@
 const express=require('express');
-const app=express();
+const mongoose = require('mongoose');
 const cors=require('cors');
 
+const url = "mongodb+srv://price_db:priceDetails@pricedetails.li01v.mongodb.net/price_db?retryWrites=true&w=majority const"
+const app=express();
 
 app.use(cors());
 app.use(express.urlencoded());
 app.use(express.json());
 
-
+///////////////////////////////////////////////////////----------server setup-----------------------/////////////////////////////
 app.get('/',function(req,res){
     res.send('Hello world!');
 });
 
 app.post('/products',function(req,res){
-    
-   
     console.log(req.body);
-  
     res.send(req.body);
 
 });
@@ -24,4 +23,19 @@ app.post('/products',function(req,res){
 app.listen(3000,function(){
     console.log('server is started in port 3000...');
 });
+//////////////////////////////////////////////////////-----------Database Connection------------///////////////////////////////////////////
 
+mongoose.connect(url,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: true
+    })
+    .then(() => {
+      console.log('Connected to database !!');
+    })
+    .catch((err)=>{
+      console.log('Connection failed !!'+ err.message);
+    });
+    
